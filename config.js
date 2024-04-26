f71cdaf4e3a1f386c952370599017788230f5227
CYBER WARRIOR <134825125+CYBER-WARRIIOR@users.noreply.github.com>
2024-04-26 12:19:02 +0500

    Update config.js

--- config.js
+++ config.js
@@ -12,17 +12,17 @@ import moment from "moment-timezone"
 
 
 global.owner = [
-  ["923140053156", "𝑪𝒀𝑩𝑬𝑹_𝑾𝑨𝑹𝑹𝑰𝑶𝑹", false],
+  ["923092668108", "Prince", false],
   [''], 
   [''] 
 ]
 //💌------------------------------------------💌
 
 
-//💌global.pairingNumber = "923140053156" //put your bot number here💌
-global.mods = ['923140053156'] 
-global.prems = ['923140053156']
-global.allowed = ['923140053156']
+//💌global.pairingNumber = "" //put your bot number here💌
+global.mods = ['923042205427'] 
+global.prems = ['']
+global.allowed = ['']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -114,22 +114,22 @@ global.princegpt = [gpt1, gpt2, gpt3, gpt4, gpt5, gpt6]
 
 
 // Moderator 
-global.developer = 'https://wa.me/message/923140053156' //contact
+global.developer = 'https://wa.me/message/DCAK67ON3XVOG1' //contact
 //💌------------------------------------------💌
 
 
 
 //Sticker WM
-global.botname = '𝑪𝒀𝑩𝑬𝑹_𝑾𝑨𝑹𝑹𝑰𝑶𝑹 '
-global.princebot = '🛡️𝑪𝒀𝑩𝑬𝑹_𝑾𝑨𝑹𝑹𝑰𝑶𝑹-𝘔𝘋🛡️'
-global.packname = '𝑪𝒀𝑩𝑬𝑹_𝑾𝑨𝑹𝑹𝑰𝑶𝑹♥️' 
-global.author = '♥️' 
+global.botname = '𝘗𝘙𝘐𝘕𝘊𝘌-𝘉𝘖𝘛-𝘔𝘋'
+global.princebot = '🛡️𝘗𝘙𝘐𝘕𝘊𝘌-𝘉𝘖𝘛-𝘔𝘋🛡️'
+global.packname = 'Prince♥️' 
+global.author = 'Prince♥️' 
 global.princeig = 'https://www.instagram.com' 
 global.princegp = 'https://chat.whatsapp.com/GWJkAJSgbv27sGOMLAzMDS'
-global.menuvid = 'https://telegra.ph/file/173eb5a7684faa975f036.mp4'
-global.Princesc = 'https://github.com/CYBER-WARRIIOR/THE-CYBER-WARRIIOR-BOT' 
+global.menuvid = 'https://i.imgur.com/GFAAXqw.mp4'
+global.Princesc = 'https://github.com/PRINCE-GDS/THE-PRINCE-BOT' 
 global.princeyt = 'https://youtube.com/'
-global.Princelog = 'https://i.imgur.com/ur24PoA.jpg'
+global.Princelog = 'https://i.imgur.com/cUvIv5w.jpeg'
 global.thumb = fs.readFileSync('./Assets/Prince.png')
 //💌------------------------------------------💌
 
