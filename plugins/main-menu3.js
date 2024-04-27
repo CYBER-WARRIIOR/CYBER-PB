8c13d5f95975edecd947223e1dcabec257376804
CYBER WARRIOR <134825125+CYBER-WARRIIOR@users.noreply.github.com>
2024-04-27 07:34:52 +0500

    𝑪𝒀𝑩𝑬𝑹_𝑾𝑨𝑹𝑹𝑰𝑶𝑹

--- plugins/main-menu3.js
+++ plugins/main-menu3.js
@@ -397,42 +397,120 @@ import {
    
     global.fcontact = { key: { fromMe: false, participant: `0@s.whatsapp.net`, remoteJid: 'status@broadcast' }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
     const infoText = `
-   🛡️ ${botname} 🛡️\n
-    Hᴀɪɪɪ ${name}👋🏻
-    
-     ${readMore}
-
-  ╭━━━━⊱⊱『 *INFO*』⊱⊱⊱━━━━━╮ 
-  │ *Rᴇᴘʟʏ ᴡɪᴛʜ ᴛʜᴇ ɴᴜᴍʙᴇʀ*
-  │ *ᴛᴏ ɢᴇᴛ ᴍᴇɴᴜ*
-  ╰───────⳹
-
-  
-  ╭━━⊱━━⊱⊱「ᴀʟʟ ᴍᴇɴᴜs」⊱⊱━━⊱━╮
-  │✫ -  *1.* ʙᴏᴛ ᴍᴇɴᴜ
-  │✫ -  *2.* ᴏᴡɴᴇʀ ᴍᴇɴᴜ
-  │✫ -  *3.* ɢʀᴏᴜᴘ ᴍᴇɴᴜ
-  │✫ -  *4.* ғᴜɴ ᴍᴇɴᴜ
-  │✫ -  *5.* ʀᴇᴀᴄᴛɪᴏɴ ᴍᴇɴᴜ
-  │✫ -  *6.* ᴅᴏᴡɴʟᴏᴀᴅᴇʀ ᴍᴇɴᴜ
-  │✫ -  *7.* ɢᴀᴍᴇ ᴍᴇɴᴜ
-  │✫ -  *8.* ʟᴏɢᴏ ᴍᴇɴᴜ
-  │✫ -  *9.* sᴛɪᴄᴋᴇʀ ᴍᴇɴᴜ
-  │✫ -  *10.* ᴀᴜᴅɪᴏ ᴍᴇɴᴜ
-  │✫ -  *11.* ɴᴇᴡs ᴍᴇɴᴜ
-  │✫ -  *12.* sᴛᴜᴅʏ ᴍᴇɴᴜ
-  │✫ -  *13.* ᴛᴏᴏʟs ᴍᴇɴᴜ
-  │✫ -  *14.* ᴀɪ ᴍᴇɴᴜ
-  │✫ -  *15.* ʀᴇʟɪɢɪᴏɴ ᴍᴇɴᴜ
-  │✫ -  *16.* ᴘʟᴜɢɪɴ ᴍᴇɴᴜ
-  ╰━━━━━━━━━━━━━━━━━━━━╯`
+   `
 ;
 
   
   const { result, key, timeout } = await conn.sendMessage(m.chat, { video: { url: menuvid }, caption: infoText.trim(),  gifPlayback: true,
   gifAttribution: 0}, { quoted: m})
   
-  // Save the menu options to gurumenu
+ ` ╭━༻𝑪𝒀𝑩𝑬𝑹_𝑾𝑨𝑹𝑹𝑰𝑶𝑹༺⊰━─
+
+
+╭━⊱「Wᴇʟᴄᴏᴍᴇ ᴛᴏ ᴄᴍᴅ ᴍᴇɴᴜ⊱━╮
+│
+│🕋✫ - 「${usedPrefix}𝘘𝘶𝘳𝘢𝘯𝘮𝘦𝘯𝘶
+│📚✫ - 「${usedPrefix}𝘚𝘵𝘶𝘥𝘺𝘮𝘦𝘯𝘶
+│🛡️✫ - 「${usedPrefix}𝘖𝘸𝘯𝘦𝘳𝘮𝘦𝘯𝘶
+│💌✫ - 「${usedPrefix}𝘉𝘰𝘵𝘮𝘦𝘯𝘶 
+│🧬✫ - 「${usedPrefix}𝘎𝘳𝘰𝘶𝘱𝘮𝘦𝘯𝘶
+│📥✫ - 「${usedPrefix}𝘋𝘭𝘮𝘦𝘯𝘶
+│🧰✫ - 「${usedPrefix}𝘛𝘰𝘰𝘭𝘮𝘦𝘯𝘶
+│🎨✫ - 「${usedPrefix}𝘚𝘵𝘪𝘤𝘬𝘦𝘳𝘮𝘦𝘯𝘶
+│🎉✫ - 「${usedPrefix}𝘍𝘶𝘯𝘮𝘦𝘯𝘶 
+│🎮✫ - 「${usedPrefix}𝘎𝘢𝘮𝘦𝘮𝘦𝘯𝘶
+│🎩✫ - 「${usedPrefix}𝘓𝘰𝘨𝘰𝘮𝘦𝘯𝘶
+│ *${usedPrefix}ᴍᴇɴᴜ2 ғᴏʀ ᴄᴏᴍᴍᴀɴᴅs ʟɪsᴛ*
+│ *${usedPrefix}ᴍᴇɴᴜ3 ғᴏʀ sᴘᴇᴄɪᴀʟ ᴍᴇɴᴜ ʟɪsᴛ*
+│ *${usedPrefix}ʟɪsᴛ ғᴏʀ ᴀʟʟ ᴄᴏᴍᴍᴀɴᴅs ʟɪsᴛ*
+│
+│ *${greeting}*
+╰━━━━━━━━━━━━━━━━━━━━━━╯
+📚 *_Quote of the day: ${quote}_* 📚
+`
+
+
+    conn.sendFile(m.chat, pp, 'perfil.jpg', str, m, false)
+    m.react(done)
+
+}
+handler.help = ['main']
+handler.tags = ['group']
+handler.command = ['menu', 'help','h','commands'] 
+
+export default handler
+function clockString(ms) {
+    let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+    let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+    let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+    return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
+    
+    function ucapan() {
+      const time = moment.tz('Asia/Karachi').format('HH')
+      let res = "happy early in the day☀️"
+      if (time >= 4) {
+        res = "Good Morning 🌄"
+      }
+      if (time >= 10) {
+        res = "Good Afternoon ☀️"
+     }
+      if (time >= 15) {
+        res = "Good Afternoon 🌇"
+      }
+      if (time >= 18) {
+        res = "Good Night 🌙"
+      }
+      return res
+    }
+    const quotes = [
+      "I'm not lazy, I'm just on my energy saving mode.",
+      "Life is short, smile while you still have teeth.",
+      "I may be a bad influence, but darn I am fun!",
+      "I'm on a whiskey diet. I've lost three days already.",
+      "Why don't some couples go to the gym? Because some relationships don't work out.",
+      "I told my wife she should embrace her mistakes... She gave me a hug.",
+      "I'm great at multitasking. I can waste time, be unproductive, and procrastinate all at once.",
+      "You know you're getting old when you stoop to tie your shoelaces and wonder what else you could do while you're down there.",
+      "I'm so good at sleeping, I can do it with my eyes closed.",
+      "If you think nobody cares if you’re alive, try missing a couple of payments.",
+      "I used to think I was indecisive, but now I'm not so sure.",
+      "If you can't convince them, confuse them.",
+      "I told my wife she was drawing her eyebrows too high. She looked surprised.",
+      "I'm not clumsy, I'm just on a mission to test gravity.",
+      "I told my wife she should do more push-ups. She said, 'I could do a hundred!' So I counted to ten and stopped.",
+      "Life is like a box of chocolates; it doesn't last long if you're hungry.",
+      "I'm not saying I'm Wonder Woman, I'm just saying no one has ever seen me and Wonder Woman in the same room together.",
+      "Why do they call it beauty sleep when you wake up looking like a troll?",
+      "I don't always lose my phone, but when I do, it's always on silent.",
+      "My bed is a magical place where I suddenly remember everything I was supposed to do.",
+      "I love the sound you make when you shut up.",
+      "I'm not arguing, I'm just explaining why I'm right.",
+      "I'm not a complete idiot, some parts are missing.",
+      "When life gives you lemons, squirt someone in the eye.",
+      "I don't need anger management. You just need to stop making me angry.",
+      "I'm not saying I'm Batman. I'm just saying no one has ever seen me and Batman in the same room together.",
+      "I'm not saying I'm Superman. I'm just saying no one has ever seen me and Superman in the same room together.",
+      "I'm not saying I'm Spider-Man. I'm just saying no one has ever seen me and Spider-Man in the same room together.",
+      "I'm not saying I'm a superhero. I'm just saying no one has ever seen me and a superhero in the same room together.",
+      "The early bird can have the worm because worms are gross and mornings are stupid.",
+      "If life gives you lemons, make lemonade. Then find someone whose life has given them vodka and have a party!",
+      "The road to success is always under construction.",
+      "I am so clever that sometimes I don't understand a single word of what I am saying.",
+      "Some people just need a high-five. In the face. With a chair.",
+      "I'm not saying I'm perfect, but I'm pretty close.",
+      "A day without sunshine is like, you know, night.",
+      "The best way to predict the future is to create it.",
+      "If you can't be a good example, then you'll just have to be a horrible warning.",
+      "I don't know why I keep hitting the escape button. I'm just trying to get out of here.",
+      "I'm not lazy. I'm on energy-saving mode.",
+      "I don't need a hairstylist, my pillow gives me a new hairstyle every morning.",
+      "I don't have a bad handwriting, I have my own font.",
+      "I'm not clumsy. It's just the floor hates me, the table and chairs are bullies, and the walls get in my way.",
+      "I'm not saying I'm Batman. I'm just saying no one has ever seen me and Batman in the same room together.",
+      "I'm not saying I'm Wonder Woman. I'm just saying no one has ever seen me and Wonder Woman in the same room together.",
+      "I'm not saying I'm Superman. I'm just saying no one has ever seen me and Superman in the same room together.",
+      "I'm not saying I'm Spider-Man. I'm just saying no one has ever seen me and Spider-Man in the same room together."
+      "I'm not saying I'm a superhero. I'm just saying no one has ever seen me and a superhero in the same room together." // Save the menu options to gurumenu
   conn.gurumenu[m.sender] = {
     result,
     key,
